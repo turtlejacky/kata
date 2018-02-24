abc525ec234870f79bd7706131fb4e94aa97a155
Neil <taojack@Jacky-2.local>
2018-02-24 16:14:12 +0800

    add rot13

---
+++ rot13/rot13/rot13.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace rot13
+{
+    public class Rot13
+    {
+        public string Rot13_helper(string input)
+        {
+            string ans = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] >= 'a' && input[i] <= 'm') || (input[i] >= 'A' && input[i] <= 'M'))
+                    ans += Convert.ToChar(input[i] + 13);
+                else if ((input[i] >= 'n' && input[i] <= 'z') || (input[i] >= 'N' && input[i] <= 'Z'))
+                    ans += Convert.ToChar(input[i] - 13);
+                else
+                    ans += input[i];
+            }
+            return ans;
+        }
+    }
+}
---
+++ rot13/rot13_test/rot13_Test.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rot13
+{
+    [TestClass]
+    public class Rot13_Test
+    {
+        [TestMethod]
+        public void Test_pre13_Character_lowerCase()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("abcdefghijklm");
+            var expect = "nopqrstuvwxyz";
+            Assert.AreEqual( result, expect );
+        }
+        [TestMethod]
+        public void Test_post13_Character_lowerCase()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("nopqrstuvwxyz");
+            var expect = "abcdefghijklm";
+            Assert.AreEqual(result, expect);
+        }
+        [TestMethod]
+        public void Test_pre13_Character_capital()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("ABCDEFGHIJKLM");
+            var expect = "NOPQRSTUVWXYZ";
+            Assert.AreEqual(result, expect);
+        }
+        [TestMethod]
+        public void Test_post13_Character_capital()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("NOPQRSTUVWXYZ");
+            var expect = "ABCDEFGHIJKLM";
+            Assert.AreEqual(result, expect);
+        }
+        [TestMethod]
+        public void Example1()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("EBG13 rknzcyr.");
+            var expect = "ROT13 example.";
+            Assert.AreEqual(result, expect);
+        }
+        [TestMethod]
+        public void Example2()
+        {
+            var rotTranslation = new Rot13();
+            var result = rotTranslation.Rot13_helper("This is my first ROT13 excercise!");
+            var expect = "Guvf vf zl svefg EBG13 rkprepvfr!";
+            Assert.AreEqual(result, expect);
+        }
+
+
+    }
+}
