6801151b7023729d78054062c8680707df729836
turtlejacky <jack370434@hotmail.com.tw>
2018-04-03 23:29:35 +0800

    add LargestSubsequences

---
+++ LargestSubsequences/LargestSubsequences/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+ 
+namespace LargestSubsequences
+{
+    public class Kata
+    {
+        public static int LargestSum(int[] arr)
+        {
+            int sumMax = 0;
+            int[] dynamicArr= new int[arr.Length+1];
+            dynamicArr[0] = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dynamicArr[i + 1] = Math.Max(dynamicArr[i] + arr[i], 0 );
+                sumMax = Math.Max(dynamicArr[i + 1], sumMax);
+            }
+            return sumMax;
+        }
+
+
+     
+    }
+}
---
+++ LargestSubsequences/LargestSubsequencesTests/KataTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LargestSubsequences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestSubsequences
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void LargestSumTest_AllNegative()
+        {
+            var expect = 0;
+            var actual = Kata.LargestSum(new[] { -1, -2, -3 });
+            Assert.AreEqual(expect,actual);
+        }
+
+        [TestMethod()]
+        public void LargestSumTest_None()
+        {
+            var expect = 0;
+            var actual = Kata.LargestSum(new int[0]);
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void LargestSumTest_1()
+        {
+            var expect = 10;
+            var actual = Kata.LargestSum(new [] { 1, 2, 3, 4 });
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void LargestSumTest_2()
+        {
+            var expect = 187;
+            var actual = Kata.LargestSum(new[] { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 });
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
