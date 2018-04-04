bebc4d441c30eeed0371fbfa80aa66520c6f5146
turtlejacky <jack370434@hotmail.com.tw>
2018-04-04 11:54:02 +0800

    add IsAnagram

---
+++ IsAnagram/IsAnagram/IsAnagram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAnagram
+{
+    public class IsAnagramClass
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            var testArr = test.ToLower().ToCharArray().OrderBy(c => c);
+            var orgArr = original.ToLower().ToCharArray().OrderBy(c => c);
+            return testArr.SequenceEqual(orgArr);
+        }
+
+    }
+}
---
+++ IsAnagram/IsAnagramAnagramTests/IsAnagramClassTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IsAnagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAnagram
+{
+    [TestClass()]
+    public class IsAnagramClassTests
+    {
+        [TestMethod()]
+        public void IsAnagramTestSorted()
+        {
+            Assert.AreEqual(true, IsAnagramClass.IsAnagram("foefet", "toffee"));
+        }
+        [TestMethod()]
+        public void IsAnagramTestUpperCase()
+        {
+            Assert.AreEqual(true, IsAnagramClass.IsAnagram("Buckethead", "buckethead"));
+        }
+        [TestMethod()]
+        public void IsAnagramTestSortedAndUpperCase()
+        {
+            Assert.AreEqual(true, IsAnagramClass.IsAnagram("Twoo", "Woot"));
+        }
+        [TestMethod()]
+        public void IsAnagramTestDifferentLength()
+        {
+            Assert.AreEqual(false, IsAnagramClass.IsAnagram("apple", "pale"));
+        }
+    }
+}
