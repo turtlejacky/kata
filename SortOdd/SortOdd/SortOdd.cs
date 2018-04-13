63f60ae1cb7b4867ce80ad60700e80f6f7938adf
turtlejacky <jack370434@hotmail.com.tw>
2018-04-13 23:40:01 +0800

    add SortOdd

---
+++ SortOdd/SortOdd/SortOdd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortOdd
+{
+    public class Kata
+    {
+        public static int[] SortArray(int[] array)
+        {
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                    continue;
+                for (var j = 0; j < array.Length; j++)
+                {
+                    if (array[j] % 2 == 0)
+                        continue;
+                    if (array[i] >= array[j]) continue;
+                    var temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ SortOdd/SortOddSortOddTests/KaraTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortOdd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortOdd
+{
+    [TestClass()]
+    public class KaraTests
+    {
+        [TestMethod()]
+        public void SortArrayTest()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+        }
+        [TestMethod()]
+        public void SortArrayTest2()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+        }
+        [TestMethod()]
+        public void SortArrayTest3()
+        {
+            CollectionAssert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
+        }
+        [TestMethod()]
+        public void SortArrayTest4()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 4, 6, 8, 10 }, Kata.SortArray(new int[] { 2,4,6,8,10}));
+        }
+    }
+}
