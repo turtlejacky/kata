8c8b63a9d6b75edde359a8332b70d535aa560ad5
turtlejacky <jack370434@hotmail.com.tw>
2018-04-22 21:41:04 +0800

    add change coins

---
+++ ChangeCoins/ChangeCoins/ChangeCoins.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCoins
+{
+    public class ChangeCoins
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            int[] dramaticArray = new int[money+1];
+            dramaticArray[0] = 1;
+            foreach (var coin in coins)
+            {
+                for (var j = coin; j <= money; j++)
+                {
+                    dramaticArray[j] = dramaticArray[j] + dramaticArray[j - coin];
+                }
+            }
+
+            return dramaticArray[money];
+        }
+    }
+}
---
+++ ChangeCoins/ChangeCoinsTests/ChangeCoinsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChangeCoins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCoins.Tests
+{
+    [TestClass()]
+    public class ChangeCoinsTests
+    {
+        [TestMethod()]
+        public void CountCombinationsTest()
+        {
+            var expect = 3;
+            var actual = ChangeCoins.CountCombinations(4, new int[] {1, 2});
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void CountCombinationsTest1()
+        {
+            var expect = 5;
+            var actual = ChangeCoins.CountCombinations(5, new int[] {1, 2, 3});
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void CountCombinationsTest2()
+        {
+            var expect = 4;
+            var actual = ChangeCoins.CountCombinations(10, new[] {5, 2, 3});
+            Assert.AreEqual(expect, actual);
+        }
+
+
+
+    }
+}
