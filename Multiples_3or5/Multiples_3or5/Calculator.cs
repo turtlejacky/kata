05ec663559bf5a3da4c84c3bcbf8582d26b0b525
turtlejacky <jack370434@hotmail.com.tw>
2018-05-12 19:10:22 +0800

    add Multiples_3or5

---
+++ Multiples_3or5/Multiples_3or5/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Multiples__3or5
+{
+    public class Calculator
+    {
+        public int SumMultiples(int n)
+        {
+            return Enumerable.Range(0, n).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+        }
+    }
+}
---
+++ Multiples_3or5/Multiples_3or5Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Multiples__3or5
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        private Calculator _calculator = new Calculator();
+        [TestMethod()]
+        public void SumMultiplesTest_23()
+        {
+            Assert.AreEqual(23, _calculator.SumMultiples(10));
+        }
+
+        [TestMethod()]
+        public void SumMultiplesTest_45()
+        {
+            Assert.AreEqual(45, _calculator.SumMultiples(15));
+        }
+    }
+}
