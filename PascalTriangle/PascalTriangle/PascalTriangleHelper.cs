5355f8cff639d505c5334db3a1d91accd3e97f66
turtlejacky <jack370434@hotmail.com.tw>
2018-05-13 01:39:55 +0800

    add PascalTriangle

---
+++ PascalTriangle/PascalTriangle/PascalTriangleHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PascalTriangle
+{
+    public class PascalTriangleHelper
+    {
+        public List<int> PascalsTriangle(int n)
+        {
+            var list = new List<int>();
+            var count = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        count = 1;
+                    else
+                        count = count * (i - j + 1) / j;
+                    list.Add(count);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ PascalTriangle/PascalTriangleTests/PascalTriangleHelperTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PascalTriangle.Tests
+{
+    [TestClass()]
+    public class PascalTriangleHelperTests
+    {
+        private PascalTriangleHelper _pascalTriangleHelper = new PascalTriangleHelper();
+
+        [TestMethod()]
+        public void PascalsTriangleTest_Raw_1()
+        {
+            var actual = _pascalTriangleHelper.PascalsTriangle(1);
+            var expected = new List<int> { 1 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void PascalsTriangleTest_Raw_3()
+        {
+            var actual = _pascalTriangleHelper.PascalsTriangle(4);
+            var expected = new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void PascalsTriangleTest_Raw_5()
+        {
+            var actual = _pascalTriangleHelper.PascalsTriangle(5);
+            var expected = new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
