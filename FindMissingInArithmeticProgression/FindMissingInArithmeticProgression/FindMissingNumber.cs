a0cc60d056f19d96d7feb7c680d55d794eeaaea5
turtlejacky <jack370434@hotmail.com.tw>
2018-05-12 18:32:29 +0800

    add FindMissingInArithmeticProgression

---
+++ FindMissingInArithmeticProgression/FindMissingInArithmeticProgression/FindMissingNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FindMissingInArithmeticProgression
+{
+    public class FindMissingNumber
+    {
+        public int FindMissing(List<int> list)
+        {
+            int gap = (list.Last() - list.First()) / list.Count;
+            return list.Where((x, index) => x != list.First() + gap * index).First() - gap;
+        }
+    }
+}
---
+++ FindMissingInArithmeticProgression/FindMissingInArithmeticProgressionTests/FindMissingNumberTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindMissingInArithmeticProgression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMissingInArithmeticProgression.Tests
+{
+    [TestClass()]
+    public class FindMissingNumberTests
+    {
+        private FindMissingNumber _finder = new FindMissingNumber();
+        [TestMethod]
+        public void FindMissingTest_Gap_2()
+        {
+            Assert.AreEqual(7,_finder.FindMissing(new List<int>() { 1, 3, 5, 9, 11 }));
+        }
+
+        [TestMethod]
+        public void FindMissingTest_Gap_5()
+        {
+            Assert.AreEqual(15, _finder.FindMissing(new List<int>() { 0, 5, 10, 20, 25 }));
+        }
+
+        [TestMethod]
+        public void FindMissingTest_Gap_180()
+        {
+            Assert.AreEqual(1400, _finder.FindMissing(new List<int>() { 1040, 1220, 1580 }));
+        }
+    }
+}
