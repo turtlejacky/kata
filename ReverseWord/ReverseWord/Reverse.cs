636300758f0bc878c120f7a5c4f6c07c627bc2da
neil.tao83 <neil.tao@titansoft.com.sg>
2018-05-19 23:40:37 +0800

    add ReverseWord

---
+++ ReverseWord/ReverseWord/Reverse.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace ReverseWord
+{
+    public class Reverse
+    {
+        public string ReverseSentence(string input)
+        {
+            var splitInput = input.Split(' ');
+            return string.Join(" ", splitInput.Select(x => new string(x.Reverse().ToArray())));
+        }
+    }
+}
---
+++ ReverseWord/ReverseWordTests/ReverseTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseWord.Tests
+{
+    [TestClass()]
+    public class ReverseTests
+    {
+        private readonly Reverse _reverseHelp = new Reverse();
+
+        [TestMethod]
+        public void ReverseSentence_LongSentence()
+        {
+            Assert.AreEqual("siht si ym !koob", _reverseHelp.ReverseSentence("this is my book!"));
+        }
+
+        [TestMethod]
+        public void ReverseSentence_Test()
+        {
+            Assert.AreEqual("cba", _reverseHelp.ReverseSentence("abc"));
+        }
+
+        [TestMethod]
+        public void ReverseSentence_Test_EndOfDot()
+        {
+            Assert.AreEqual(".cba", _reverseHelp.ReverseSentence("abc."));
+        }
+
+        [TestMethod]
+        public void ReverseSentence_Test_EndOfSpace()
+        {
+            Assert.AreEqual("cba ", _reverseHelp.ReverseSentence("abc "));
+        }
+    }
+}
