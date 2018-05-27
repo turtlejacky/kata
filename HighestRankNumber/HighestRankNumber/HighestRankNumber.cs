6a31b883e586052d95e90b04790f17357afb0e44
neil.tao83 <neil.tao@titansoft.com.sg>
2018-05-27 10:51:45 +0800

    add HighestRankNumber

---
+++ HighestRankNumber/HighestRankNumber/HighestRankNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace HighestRankNumber
+{
+    public class HighestRankNumber
+    {
+        public int HighestRank(int[] arr)
+        {
+            //before
+            //var frequencyArr = arr.GroupBy(x => x).Select(x => new { Num = x.Key, Count = x.Count() });
+            //return frequencyArr.OrderByDescending(x => x.Count).ThenByDescending(n => n.Num).First().Num;
+
+            return arr.GroupBy(x => x).OrderBy(x => x.Count()).ThenBy(x => x.Key).Last().Key;
+        }
+    }
+}
---
+++ HighestRankNumber/HighestRankNumberTests/HighestRankNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HighestRankNumber.Tests
+{
+    [TestClass()]
+    public class HighestRankNumberTests
+    {
+        private HighestRankNumber helper = new HighestRankNumber();
+
+        [TestMethod()]
+        public void Test_SameFrequency_Take_Bigger()
+        {
+            Assert.AreEqual(8, helper.HighestRank(new int[] { 1, 2, 3, 3, 4, 4, 4, 4, 7, 8, 8, 8, 8 }));
+        }
+
+        [TestMethod()]
+        public void Test_Simple_Case()
+        {
+            Assert.AreEqual(8, helper.HighestRank(new int[] { 1, 2, 3, 3, 4, 7, 8, 8, 8, 8 }));
+        }
+    }
+}
