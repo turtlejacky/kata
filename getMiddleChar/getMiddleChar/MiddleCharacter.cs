0ff251d0f92a15e49c8e749f07aebe110f85b9fd
neil.tao83 <neil.tao@titansoft>
2018-07-29 14:12:12 +0800

    add MiddleCharacter

---
+++ getMiddleChar/getMiddleChar/MiddleCharacter.cs
@@ -0,0 +1,29 @@
+﻿namespace getMiddleChar
+{
+    public static class MiddleCharacter
+    {
+        public static string GetMiddleCharacter(this string s)
+        {
+            if (s.Length < 3)
+            {
+                return s;
+            }
+            return LengthIsOdd(s) ? OneMiddleWord(s) : TwoMiddleWords(s);
+        }
+
+        private static bool LengthIsOdd(string s)
+        {
+            return s.Length % 2 == 1;
+        }
+
+        private static string OneMiddleWord(string s)
+        {
+            return s.Substring(s.Length / 2, 1);
+        }
+
+        private static string TwoMiddleWords(string s)
+        {
+            return s.Substring(s.Length / 2 - 1, 2);
+        }
+    }
+}
---
+++ getMiddleChar/getMiddleCharTests/MiddleCharacterTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace getMiddleChar.Tests
+{
+    [TestClass()]
+    public class MiddleCharacterTests
+    {
+        [DataRow("test", "es", DisplayName = "Length_is_even")]
+        [DataRow("testing", "t", DisplayName = "Length_is_Odd")]
+        [DataRow("A", "A", DisplayName = "Length_is_1")]
+        [DataRow("Hi", "Hi", DisplayName = "Length_is_2")]
+        [DataTestMethod]
+        public void Get_middle_character(string input, string expected)
+        {
+            Assert.AreEqual(expected, input.GetMiddleCharacter());
+        }
+    }
+}
